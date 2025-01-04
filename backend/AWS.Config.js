726542659186b15e1a7e2c2a3e1e76f535df04ec
karan-agarwal-TC <karan.agarwal@techcarrel.com>
2025-01-05 01:01:00 +0530

    Delete API logic Fixed

--- backend/AWS.Config.js
+++ backend/AWS.Config.js
@@ -2,12 +2,15 @@ const { DynamoDBClient } = require('@aws-sdk/client-dynamodb')
 const { S3Client, GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 
+// const AWSconfig = {
+//   region: process.env.region,
+//   credentials: {
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+//   }
+// }
 const AWSconfig = {
   region: process.env.region,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  }
 }
 
 const S3 = new S3Client(AWSconfig)
