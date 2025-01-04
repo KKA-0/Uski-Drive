14001c3588510a9e8d0e47b649c6e52cac8ea315
Karan Agarwal <karanyobro@gmail.com>
2025-01-05 01:41:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use('/upload', uploadRoute)
 app.use('/auth', authRoute)
 app.use('/user', userRoute)
 app.use('/', (req, res) => {
-    res.send("Beep... Beep... 🤖 hehe")
+    res.send("Beep... Beep... 🤖")
 })
 
 //app.listen(port, () => console.log("Beep... Beep... 🤖", {port}))
