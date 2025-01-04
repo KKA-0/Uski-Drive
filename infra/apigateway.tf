938c8875c94205658318774386e5a7f757d241b4
jadonharsh109 <jadonharsh109@gmail.com>
2025-01-04 16:01:23 +0530

    api gateway added

---
+++ infra/apigateway.tf
@@ -0,0 +1,36 @@
+resource "aws_apigatewayv2_api" "my_http_api" {
+  name          = "UskiDrive-API"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.my_http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.test_lambda.invoke_arn
+  passthrough_behavior   = "WHEN_NO_MATCH"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id    = aws_apigatewayv2_api.my_http_api.id
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "api_stage" {
+  api_id      = aws_apigatewayv2_api.my_http_api.id
+  name        = "default"
+  auto_deploy = true
+}
+output "api_endpoint" {
+  value = aws_apigatewayv2_stage.api_stage.invoke_url
+}
+
+resource "aws_lambda_permission" "allow_apigateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.my_http_api.execution_arn}/*/*"
+}
